ef8cd287bec0094d7d950101bd98ddfb630d5f8f
Amr Sabbagh <amr.sabbagh35@gmail.com>
2023-09-26 00:55:02 +0300

    fixed upper bg

--- lib/screens/home/widgets/home_banner.dart
+++ lib/screens/home/widgets/home_banner.dart
@@ -10,56 +10,59 @@ class HomeBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: Responsive.isMobile(context) ? 2.5 : 3,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.asset(
-            "assets/images/bg.jpg",
-            fit: BoxFit.cover,
-          ),
-          Container(color: darkColor.withOpacity(0.66)),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Discover my Creative \n Personal Space!",
-                  style: Responsive.isDesktop(context)
-                      ? Theme.of(context).textTheme.displaySmall!.copyWith(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          )
-                      : Theme.of(context).textTheme.headlineSmall!.copyWith(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                ),
-                if (Responsive.isMobileLarge(context))
-                  const SizedBox(height: defaultPadding / 2),
-                const MyBuildAnimatedText(),
-                const SizedBox(height: defaultPadding),
-                if (!Responsive.isMobileLarge(context))
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: TextButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: defaultPadding * 2,
-                          vertical: defaultPadding),
-                      backgroundColor: primaryColor,
-                    ),
-                    child: const Text(
-                      "EXPLORE NOW",
-                      style: TextStyle(color: darkColor),
-                    ),
-                  ),
-              ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: AspectRatio(
+        aspectRatio: Responsive.isMobile(context) ? 2.5 : 3,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.asset(
+              "assets/images/bg.jpg",
+              fit: BoxFit.cover,
             ),
-          )
-        ],
+            Container(color: darkColor.withOpacity(0.66)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Discover my Creative \n Personal Space!",
+                    style: Responsive.isDesktop(context)
+                        ? Theme.of(context).textTheme.displaySmall!.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            )
+                        : Theme.of(context).textTheme.headlineSmall!.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                  ),
+                  if (Responsive.isMobileLarge(context))
+                    const SizedBox(height: defaultPadding / 2),
+                  const MyBuildAnimatedText(),
+                  const SizedBox(height: defaultPadding),
+                  if (!Responsive.isMobileLarge(context))
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: defaultPadding * 2,
+                            vertical: defaultPadding),
+                        backgroundColor: primaryColor,
+                      ),
+                      child: const Text(
+                        "EXPLORE NOW",
+                        style: TextStyle(color: darkColor),
+                      ),
+                    ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
